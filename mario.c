03485c982bd0bbb915e26fc1228a283317b27804
Andrew M <134089657+andrewmesquita@users.noreply.github.com>
2023-05-27 22:01:18 -0300

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+      *
+     **
+    ***
+   ****
+
+*/
+
+int main (){
+    int i, n;
+
+    printf("difite o valor de n: ");
+    scanf("%d", &n)
+
+    for(i = 1; <= n; i++) {
+        for( j = n - i; j >= 1; j--)
+        printf(" ")
+        for(j = 1; j <= i; j++) 
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
